84b17e051e0d7f6d10d30244de1e8001e5f215dd
Edward <gonthaedward@gmail.com>
2024-07-22 16:26:11 +0700

    progress until before the post list

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,22 @@
+import menu from './assets/menu.png'
+import grid from './assets/grid.png'
 function Body(){
     return(
         <>
-          <div className="w-full block flex-grow bg-very-dark-blue text-white">
-            asd
-            </div>      
+        <hr className="h-px border-0 bg-gray-700"></hr>
+        <div className='flex items-center ml-6 mt-2'>
+            <img src={grid} className='w-4 h-4 m-1'></img>
+            <img src={menu} className='w-3 h-3 m-1'></img>
+            <div className='flex items-center justify-center w-full text-gray-400'>
+            <p className='ml-7 italic'>JavaScript</p>
+            <p className='ml-7 italic'>DevOps</p>
+            <p className='ml-7 italic'>Cloud</p>
+            <p className='ml-7 italic'>Terraform</p>
+            <p className='ml-7 italic'>Architecture</p>
+            <p className='ml-7 italic'>Scalability</p>
+            <p className='ml-7 italic'>Explainers</p>
+            </div>
+        </div>
         </>
     );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,27 +1,44 @@
 import './Header.css'
+import homeIcon from './assets/web-icon.png'
+
+
 
 function Header(){
     return(
         <>
             <header>
-                <nav className="flex items-center justify-between flex-wrap bg-very-dark-blue p-6">
-                    <div className='flex items-center flex-shrink-0 mr-6 bg-red-100'>
-                        KODOK
+                <nav className="flex items-center justify-between flex-wrap p-6">
+                    <div className='flex items-center flex-shrink-0 mr-6'>
+                        <a className='text-white' href='/'>writings.dev</a>
                     </div>
                     {/* below is for menu dropdown when mobile */}
-                    <div className='block lg:hidden text-white'>
+                    {/* <div className='block lg:hidden text-white'>
                         <button>Menu</button>
-                    </div>
+                    </div> */}
                     {/* below is for menu when on web and menu dropdown when mobile */}
                     <div className='w-full block flex-grow justify-end lg:flex lg:items-center lg:w-auto text-white'>
                         <div className='text-sm lg:flex-grow flex justify-end'>
-                            <a href="home" className='mr-4'>Categories</a>
-                            <a href="articles" className='mr-4'>Articles</a>
-                            <a href="about" className='mr-4'>About</a>
+                            <a href="categories" className='mr-4'>categories</a>
+                            <a href="articles" className='mr-4'>articles</a>
+                            <a href="about" className='mr-4'>about</a>
                         </div>
                     </div>
                 </nav>
+                <div className="w-full block flex-grow text-white">
+                    <div className="flex justify-center items-center">
+                        <img src={homeIcon} className='w-36 h-36'></img>
+                    </div>      
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className= 'font-sans italic text-5xl text-red-400 mb-2'>Writings for Developers</h1>
+                        <h2 className='font-sans text-lg'>Curated collection of articles for busy Developers</h2>
+                    </div>
+                    <div className="flex justify-center items-center">
+                    </div>
+                </div>
             </header>
+            <br></br>
+            <br></br>
+            <br></br>
         </>
     );
 }
